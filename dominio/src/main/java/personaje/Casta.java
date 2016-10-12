37c6e1e91bcd17aa8c61e4d2833aa3ff30acb894
Julian Velazquez <julycabj22@gmail.com>
2016-10-12 12:08:40 -0300

    Se ha agregado la clase casta

---
+++ dominio/src/main/java/personaje/Casta.java
@@ -0,0 +1,14 @@
+package personaje;
+
+/**
+ * IDEA I ----> CONSTA QUE HAYA SOLO HAYA DOS TIPOS DE CASTA
+ * BUENOS  Y MALOS.....
+ */
+
+
+public abstract class Casta {
+	
+	public abstract String getCasta();
+	public abstract int bonusDeDefensa();
+	public abstract int bonusDeAtaque();
+}

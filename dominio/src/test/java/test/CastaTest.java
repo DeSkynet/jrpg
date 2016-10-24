9d70fa441e7ec8d4827e6c7634f9215589b68277
JulyMorga <julian94.-@hotmail.com>
2016-10-24 17:02:06 -0300

    Se arregla Test.

--- dominio/src/test/java/test/CastaTest.java
+++ dominio/src/test/java/test/CastaTest.java
@@ -17,19 +17,19 @@ public class CastaTest {
 	//Historia de Usuario 02-Elegir Casta
 	@Test
 	public void creoUnaCastaYVeoElCambioEnSusAtributos() {
-		Personaje bender = new Robot(new Ladron());
+		Personaje bender = new Robot(new Ladron(), "ladro1");
 		Assert.assertEquals(7, bender.obtenerPuntosDeDefensa());
 	}
 	
 	@Test
 	public void creoUnaCasta() {
-		Personaje aliBaba = new Humano(new Ladron());
+		Personaje aliBaba = new Humano(new Ladron(),"ladro1");
 		Assert.assertEquals("Ladron", aliBaba.casta.getCasta());
 	}
 	
 	@Test
 	public void creoUnaCastaYVeoSuComportamiento() {
-		Personaje aliBaba = new Humano(new Ladron());
+		Personaje aliBaba = new Humano(new Ladron(),"ladro1");
 		
 		///OBTENGO EL NOMBRE DE LA CASTA
 		Assert.assertEquals("Ladron", aliBaba.casta.getCasta());
@@ -41,13 +41,13 @@ public class CastaTest {
 	
 	@Test
 	public void veoAltura() {
-		Personaje aliBaba = new Humano(new Ladron());
+		Personaje aliBaba = new Humano(new Ladron(),"ladro1");
 		Assert.assertEquals(180, aliBaba.getAltura());
 	}
 	
 	@Test
 	public void defensaDeLaCasta() {
-		Personaje bender = new Robot(new Ladron());
+		Personaje bender = new Robot(new Ladron(),"ladro1");
 		Assert.assertEquals(5+2, bender.obtenerPuntosDeDefensa());
 	}
 	

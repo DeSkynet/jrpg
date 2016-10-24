52da26484b0215ca61778a2135f40d5a65ac359e
Julian Velazquez <julycabj22@gmail.com>
2016-10-23 22:03:27 -0300

    Se ha modificado la clase Hechicero

--- dominio/src/main/java/personaje/castas/Hechicero.java
+++ dominio/src/main/java/personaje/castas/Hechicero.java
@@ -3,21 +3,24 @@ package personaje.castas;
 import personaje.Casta;
 
 public class Hechicero extends Casta{
-
+	
 	@Override
 	public String getCasta() {
 		return "Hechicero";
 	}
 
 	@Override
-	public int bonusDeDefensa() {
+	public int bonusDeDestreza() {
 		return 6;
 	}
 
 	@Override
-	public int bonusDeAtaque() {
+	public int bonusDeFuerza() {
 		return 8;
 	}
 	
-
+	@Override
+	public int bonusDeInteligencia(){
+		return 10;
+	}
 }

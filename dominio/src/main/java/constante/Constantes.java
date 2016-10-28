1a6867b1f332d3105c76066688bb82b4ae294308
William Leandro <royalty2012wla@gmail.com>
2016-10-28 14:07:14 -0300

    Se agrega paquete de constantes.

---
+++ dominio/src/main/java/constante/Constantes.java
@@ -0,0 +1,8 @@
+package constante;
+
+public class Constantes {
+	
+	/// CONSTANTES TEMPORALES	
+	public static final int[] EXPERERIENCIA_TOPE_POR_NIVEL = {0,100,300,600,1100,1800,2700,3900,5400,7200};
+	public static final int EXPERIENCIA_POR_VICTORIA = 100;
+}

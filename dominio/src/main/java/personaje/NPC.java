bcc156bf50f63f7496746f2745d95599b7f8d777
juliancarroccio <juliancarroccio@gmail.com>
2016-10-28 00:00:17 -0300

    Se agregaron cambios para incorporar NPC en Alianzas

--- dominio/src/main/java/personaje/NPC.java
+++ dominio/src/main/java/personaje/NPC.java
@@ -1,14 +1,50 @@
 package personaje;
 
-import interfaces.Atacable;
+public class NPC extends Personaje {
+	
+	static final int[][] NIVELES = {{5,5},{7,7},{9,9},{11,11},{13,13},{15,15},{17,17}}; /// ESTA MATRIZ SE PUEDE SEGUIR Y CAMBIAR VALORES, DEFINE LA DEFENZA Y EL ATAQUE DEL NPC SEGUN SU NIVEL PONER EN CLASE CONSTANTES CUAND ESTE ECHA
+	static final int NIVEL_MAX = 7;
+	
+	private int id;
+	
+	
+	public NPC(int id){
+		this.id = id;
+	}
 
-public class NPC implements Atacable {
 	
-	int salud = 100;
+	//DEFENSA
+	public int calcularPuntosDeDefensa(){
+		return NIVELES[this.nivel-1][1];
+	}
+	public int obtenerPuntosDeDefensa() {
+		return calcularPuntosDeDefensa();
+	}
 	
-	@Override
-	public void serAtacado(int daño) {
-		this.salud-= daño;
+	//ATAQUE
+	public int calcularPuntosDeAtaque(){
+		return NIVELES[this.nivel-1][0];
+	}
+	
+	public int obtenerPuntosDeAtaque() {
+		return calcularPuntosDeAtaque();
+	}
+	
+
+	public boolean puedeAtacar() {
+		return this.energia >= calcularPuntosDeAtaque();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getSalud() {
+		return salud;
 	}
 
 }

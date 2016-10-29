506234fed0014adb250fd45049e3e9c42bb3c94c
Julian Velazquez <julycabj22@gmail.com>
2016-10-28 23:33:16 -0300

    Se ha agregado la clase Constantes

--- dominio/src/main/java/constante/Constantes.java
+++ dominio/src/main/java/constante/Constantes.java
@@ -5,4 +5,6 @@ public class Constantes {
 	/// CONSTANTES TEMPORALES	
 	public static final int[] EXPERERIENCIA_TOPE_POR_NIVEL = {0,100,300,600,1100,1800,2700,3900,5400,7200};
 	public static final int EXPERIENCIA_POR_VICTORIA = 100;
+	public static final int NIVEL_MAXIMO = 9;
+	public static final int SUBA_DE_ENERGIA_DESPUES_DE_ATAQUE = 20;
 }

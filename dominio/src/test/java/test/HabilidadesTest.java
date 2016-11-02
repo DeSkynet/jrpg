c8e3ed0f9422c291170dff0e928bcefaadc055ba
juliancarroccio <juliancarroccio@gmail.com>
2016-11-02 00:18:33 -0300

    Se agrego test de habilidad Alentizar

--- dominio/src/test/java/test/HabilidadesTest.java
+++ dominio/src/test/java/test/HabilidadesTest.java
@@ -3,37 +3,37 @@ package test;
 import org.junit.Assert;
 import org.junit.Test;
 
+import habilidades.Alentizar;
 import personaje.*;
+import personaje.castas.Hechicero;
+import personaje.castas.Luchador;
 
 public class HabilidadesTest {
 	
-	//Historia de ususario 10 - Habilidad Alien
 	@Test
-	public void habilidadAlien() {
-		Personaje kodos = new Alien();
-		Personaje kang = new Alien();
-		
-		kodos.atacar(kang);
-		//kodos ataca a kang, kang queda con 90 de salud y kodos se cura un 10% de lo que ataco, pero como su salud esta al maximo queda de esa manera
-		Assert.assertEquals(93, kang.getSalud());
-		Assert.assertEquals(true, kodos.esSaludMaxima());
-		//ahora kang ataca a kodos y recupera 10% de energia
-		kang.atacar(kodos);
-		Assert.assertEquals(93 + 1, kang.getSalud());
+	public void LanzoHabilidadAPersonaje() {
+		Personaje ironMan = new SuperHeroe(new Luchador(), "IronMan");
+		Assert.assertEquals(4 + 5, ironMan.getDestreza());
+		new Alentizar().afectar(ironMan);
+		Assert.assertEquals( 4 + 5 - 5, ironMan.getDestreza());
 	}
 	
-	//Historia de ususario 11-Habilidad SuperHeroe
-		@Test
-		public void habilidadSuperHeroe() {
-			Personaje ironMan = new SuperHeroe();
-			Personaje thor = new SuperHeroe();
-			
-			Assert.assertEquals(20, thor.obtenerPuntosDeAtaque());
-			
-			//ironMan ataca a thor, entonces su ataque aumenta en 1
-			ironMan.atacar(thor);
-			Assert.assertEquals(21, thor.obtenerPuntosDeAtaque());
-		}
+	@Test
+	public void AgregoHabilidad() {
+		Personaje vivi = new Humano(new Hechicero(), "Vivi");
+		Assert.assertEquals(0, vivi.casta.getCantidadDeHabilidades());
+		vivi.casta.agregarHabilidad("Alentizar", new Alentizar());
+		Assert.assertEquals(1, vivi.casta.getCantidadDeHabilidades());
+	}
 
+	@Test
+	public void LanzoHabilidadPorNombre() {
+		Personaje radagast = new Humano(new Hechicero(), "Radagast");
+		radagast.casta.agregarHabilidad("Alentizar", new Alentizar());
+		Personaje ironMan = new SuperHeroe(new Luchador(), "IronMan");
+		Assert.assertEquals(4 + 5, ironMan.getDestreza());
+		radagast.casta.usarHabilidad("Alentizar", ironMan);
+		Assert.assertEquals( 4 + 5 - 5, ironMan.getDestreza());
+	}
 
 }

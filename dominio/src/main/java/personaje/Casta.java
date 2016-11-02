da7bd8493323ba3f07218cfb4eedcdbbd9d9f931
juliancarroccio <juliancarroccio@gmail.com>
2016-11-02 00:09:26 -0300

    Se agrego la lista de habilidades por casta

--- dominio/src/main/java/personaje/Casta.java
+++ dominio/src/main/java/personaje/Casta.java
@@ -1,5 +1,10 @@
 package personaje;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import habilidades.Habilidad;
+
 /**
  * IDEA I ----> CONSTA QUE HAYA SOLO HAYA DOS TIPOS DE CASTA
  * BUENOS  Y MALOS.....
@@ -7,6 +12,21 @@ package personaje;
 
 
 public abstract class Casta {
+	
+	private Map<String, Habilidad> habilidades = new HashMap<String,Habilidad>();
+	
+	public void agregarHabilidad(String nombreHabilidad, Habilidad habilidad) {
+		this.habilidades.put(nombreHabilidad, habilidad);
+	}
+	
+	public int getCantidadDeHabilidades() {
+		return this.habilidades.size();
+	}
+
+	public void usarHabilidad(String nombreHabilidad, Personaje personaje) {
+		this.habilidades.get(nombreHabilidad).afectar(personaje);
+		
+	}
 	
 	public abstract String getCasta();
 	public abstract int bonusDeDestreza();

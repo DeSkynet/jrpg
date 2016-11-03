2c24cd1035b404c4f0125d9d36b3018def7ad672
Ajaya, William <royalty2012wla@gmail.com>
2016-11-03 19:01:04 -0300

    Cambios menores en VentanaRegistro

--- cliente/src/main/java/ventana/VentanaRegistro.java
+++ cliente/src/main/java/ventana/VentanaRegistro.java
@@ -1,18 +1,16 @@
 package ventana;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
+@SuppressWarnings("serial")
 public class VentanaRegistro extends JFrame {
 
 	private VentanaPrincipal ventanaPrincipal;

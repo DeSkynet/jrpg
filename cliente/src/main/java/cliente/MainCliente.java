1902e0f7004b3e96510e69b50db18d5afb078d57
Ajaya, William <royalty2012wla@gmail.com>
2016-11-10 13:33:05 -0300

    Lanzador del Cliente.

--- cliente/src/main/java/cliente/MainCliente.java
+++ cliente/src/main/java/cliente/MainCliente.java
@@ -15,27 +15,38 @@ public class MainCliente {
         String usuario;
         String contraseña;
         
-        ///ESTA MUYYYY PRECARIOO! HAY QUE MODIFICAR
-        EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VentanaPrincipal frame = new VentanaPrincipal();
-					frame.setVisible(true);
-					
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+        cliente = new Cliente("nombre");
         
-        cliente = new Cliente("nombre");        
+       HiloCliente hiloCliente = new HiloCliente(cliente.getSocket());
+       hiloCliente.start();
+        VentanaPrincipal principal = new VentanaPrincipal(cliente);
+        principal.setVisible(true);
+      
         
-        HiloCliente threadCliente = new HiloCliente(cliente.getSocket()); //creo el hilo Cliente y le mando el socket. Espera un mensaje.
-        threadCliente.start(); //inicializo el hilo, ejecuta el run.
-//        cliente.elegirPersonaje(raza, casta);
-//        cliente.eligeMapa();  //le dice al servidor en que sala quiere estar.
         
-//        cliente.enviarMensaje();
-        cliente.cerrarCliente();
+
+        
+//        ///ESTA MUYYYY PRECARIOO! HAY QUE MODIFICAR
+//        EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					VentanaPrincipal frame = new VentanaPrincipal();
+//					frame.setVisible(true);
+//					
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//        
+//        cliente = new Cliente("nombre");        
+//        
+//        HiloCliente threadCliente = new HiloCliente(cliente.getSocket()); //creo el hilo Cliente y le mando el socket. Espera un mensaje.
+//        threadCliente.start(); //inicializo el hilo, ejecuta el run.
+////        cliente.elegirPersonaje(raza, casta);
+////        cliente.eligeMapa();  //le dice al servidor en que sala quiere estar.
+//        
+////        cliente.enviarMensaje();
+//        cliente.cerrarCliente();
     }
 }

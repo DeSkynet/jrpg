9a6cd90f7fa3138981eaee473caabf9ae1e15213
Ajaya, William <royalty2012wla@gmail.com>
2016-11-10 10:44:10 -0300

    Implementacion DAO para BD.

---
+++ servidor/src/main/java/dao/DAO.java
@@ -0,0 +1,14 @@
+package dao;
+
+import java.sql.SQLException;
+
+
+// SUJETO A CAMBIOS
+public abstract class DAO<T> {
+	public abstract void insertar(String user,String pass,T obj) throws SQLException;
+	public abstract void actualizar(T obj) throws SQLException;
+	public abstract boolean buscar(String user) throws SQLException;
+	public abstract void borrar(String user) throws SQLException;
+	public abstract void listarDatos() throws SQLException;
+	public abstract void cerrar();
+}
---
+++ servidor/src/main/java/dao/JugadorDAO.java
@@ -0,0 +1,101 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import personaje.Personaje;
+
+public class JugadorDAO extends DAO<Personaje>{
+	private Connection conexion = null;
+	private PreparedStatement statement;
+	private static final String PATH_CONNECTION = "jdbc:sqlite:bd/JuegoBD.bd";
+	private static final String INSERTAR = "Insert into Jugador values(?,?,?,?,?);";
+	private static final String BUSCAR = "select *from Jugador where Usuario = ?;";
+	private static final String ACTUALIZAR = "update Jugador set Experiencia = ? where Usuario = ?;";
+	private static final String ELIMINAR = "delete from Jugador where Usuario = ?;";
+	private static final String VER_TODOS = "select *from Jugador;";
+	
+	public JugadorDAO() {
+		if(conexion==null) {
+			try {
+				conexion = DriverManager.getConnection(PATH_CONNECTION);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	public void insertar(String user, String pass, Personaje obj) throws SQLException {
+		statement = conexion.prepareStatement(INSERTAR);
+		
+		statement.setString(1, user);
+		statement.setString(2, pass);
+		statement.setDouble(3, obj.getExperiencia());
+		statement.setString(4, "Raza");
+		statement.setString(5, obj.getCasta().getCasta());
+		
+		statement.execute();
+		
+	}
+
+	@Override
+	public void actualizar(Personaje obj) throws SQLException {
+		statement = conexion.prepareStatement(ACTUALIZAR);
+		
+		//ESTO SOLO ES PRUEBA ---> NO EXAMINAR
+		statement.setDouble(1, 34);
+		statement.setString(2, "Julio Perez");
+		
+		statement.executeUpdate();
+		
+	}
+
+	@Override
+	public boolean buscar(String user) throws SQLException {
+		statement = conexion.prepareStatement(BUSCAR);
+		statement.setString(1, user);
+		ResultSet res = statement.executeQuery();
+		
+		if(!res.next())
+			return false;
+		
+		return true;
+	}
+
+	@Override
+	public void borrar(String user) throws SQLException {
+		statement = conexion.prepareStatement(ELIMINAR);
+		
+		statement.setString(1, user);
+		
+		statement.executeUpdate();
+		
+	}
+	
+	//podria devolver un ArrayList de Jugadores...
+	@Override
+	public void listarDatos() throws SQLException {
+		ResultSet res;
+		statement = conexion.prepareStatement(VER_TODOS);
+		
+		res = statement.executeQuery();
+		
+		while(res.next()) {
+			System.out.println(res.getString(1) + "|" + res.getString(2) + "|"  + res.getDouble(3) + "|" + res.getString(4) + "|" + res.getString(5));
+		}
+	}
+
+	@Override
+	public void cerrar() {
+		try {
+			conexion.close();
+		} catch (SQLException e) {
+			e.getSQLState();
+		}
+	}
+
+}

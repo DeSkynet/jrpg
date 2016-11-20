6c4e2faaa51e70c49a54ef0e0c02f356c0ef95e9
Julian Velazquez <julycabj22@gmail.com>
2016-11-20 16:08:05 -0300

    Se agrego la clase estado, donde muestra el estado de la interfaz grafica del juego

---
+++ cliente/src/main/java/estado/Estado.java
@@ -0,0 +1,29 @@
+package estado;
+
+import java.awt.Graphics;
+
+import estado.Estado;
+import pantalla.Juego;
+
+public abstract class Estado {
+
+	private static Estado estadoActual = null; // Estado para saber que actualizacion realizar
+ 
+	protected Juego juego;
+	
+	public Estado(Juego juego) {
+		this.juego = juego;
+	}
+	
+	public abstract void actualizar();
+	
+	public abstract void graficar(Graphics g);
+	
+	public static void setEstado(Estado estado) {
+		estadoActual = estado;
+	}
+	
+	public static Estado getEstado() {
+		return estadoActual;
+	}
+}
---
+++ cliente/src/main/java/estado/EstadoJuego.java
@@ -0,0 +1,44 @@
+package estado;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import graficos.Mapa;
+import graficos.Sprite;
+import herramientas.Constantes;
+import pantalla.Ente;
+import pantalla.Juego;
+
+/*
+*	ESTE METODO ES EL NEXO DE LOS MAPAS Y PERSONAJES CON EL JUEGO.
+*/
+
+ 
+public class EstadoJuego extends Estado {
+	private Mapa mapa;
+	private Ente ente;
+	
+	public EstadoJuego(Juego juego, Mapa mapa) {
+		super(juego);
+		this.mapa = mapa;
+		ente = new Ente(juego, mapa, 128 , 128, 0, 0 , Sprite.mago, 200);
+	}
+	
+	@Override
+	public void actualizar() {
+		mapa.actualizar();
+		ente.actualizar();
+	}
+
+	@Override
+	public void graficar(Graphics g) {
+		g.setColor(Color.BLACK);
+		g.fill3DRect(0, 0, Constantes.ANCHO_PANTALLA, Constantes.ALTO_PANTALLA, false);
+		mapa.graficar(g);
+		ente.graficar(g);
+		
+	}
+	
+	
+
+}
